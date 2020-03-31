d5589d4002ea256cdbc4e9fa80c5b94f317ddd21
The32bitguy <35638613+The32bitguy@users.noreply.github.com>
2020-03-31 19:37:40 +0100

    Update init.lua with new content

--- init.lua
+++ init.lua
@@ -1,26 +1,25 @@
 
 -- tobbaco
-farming.register_plant("tobacco:leaves", {
-	description = ("tobacco seeds"),
-	harvest_description = ("tobacco leaves"),
-	inventory_image = "tobacco_seed.png",
+farming.register_plant("tobbaco:leaves", {
+	description = ("Tobbaco seeds"),
+	harvest_description = ("tobbaco leaves"),
+	inventory_image = "tobbaco_seed.png",
 	steps = 7,
 	minlight = 13,
 	maxlight = default.LIGHT_MAX,
 	fertility = {"grassland", "desert"},
-	groups = {drug = 1,flammable = 4}, --A new item group that will probably not be used for anything
-	custom_growth = {optimum_heat = 75, heat_scaling = "exponential", heat_a = 10, heat_b = 1.2, heat_base_speed = 2500, variance = 500},
+	groups = {tobbaco = 1,flammable = 4}, --A new item group that will probably not be used for anything
 	})
 -- This item emits smoke when used
-minetest.register_craftitem("tobacco:cigarette", {
-    description = "cigarette",
-    inventory_image = "tobacco_cigarette.png",
+minetest.register_craftitem("tobbaco:cigarette", {
+    description = "Cigarette",
+    inventory_image = "tobbaco_cigarette.png",
+    groups = {tobbaco = 1},
     on_use = function(itemstack, player, pointed_thing)
-
-    minetest.chat_send_player(player:get_player_name(),"You smoke the cigarette")
-        smokepos = player:get_pos()
-    minetest.add_particlespawner({
-	amount = 13,
+    minetest.chat_send_player(player:get_player_name(),"You smoke the cigarette") 
+    smokepos = player:get_pos() 
+      minetest.add_particlespawner({
+	amount = 16,
 	time = 1,
 	minpos = {x=smokepos.x, y=smokepos.y+1.5,z=smokepos.z},
 	maxpos = {x=smokepos.x, y=smokepos.y+1.5,z=smokepos.z},
@@ -34,47 +33,106 @@ minetest.register_craftitem("tobacco:cigarette", {
 	maxsize = 1.5,
 	collisiondetection = true,
 	vertical = false,
-	texture = "tobacco_smoke." .. math.random(1,3) .. ".png",
+	texture = "tobbaco_smoke." .. math.random(1,3) .. ".png",
 })
---Plays one of three sounds to player who rolled
+--Plays one of two sounds at the player who smoked position
     minetest.sound_play("smoke", {
-    to_player = player,
+    pos = {x=smokepos.x, y=smokepos.y, z=smokepos.z},
+	max_hear_distance = 10,
 	gain = 6.0,
 })
-        return "default:paper"
+-- Replaces the cigarette with a recyclable waste product
+        return "tobbaco:cigbutt"
     end,
 })
 
-minetest.register_craftitem("tobacco:leaves_cured", {
-    description = "dried tobacco leaves",
-    inventory_image = "tobacco_cured.png",
+-- These can be chewed, or made into snuff or cigarettes
+minetest.register_craftitem("tobbaco:leaves_cured", {
+    description = "Dried tobbaco leaves",
+    inventory_image = "tobbaco_cured.png",
+    groups = {tobbaco = 1},
+    on_use = function(itemstack, player, pointed_thing)
+    smokepos = player:get_pos()
+    minetest.chat_send_player(player:get_player_name(),"You chew the tobbaco") 
+   
+--Plays one of two sounds at the player who smoked position
+    minetest.sound_play("chew", {
+    pos = {x=smokepos.x, y=smokepos.y, z=smokepos.z},
+	max_hear_distance = 8,
+	gain = 5.0,
+})
+itemstack:take_item()
+    return itemstack
+end
 })
 
-
+minetest.register_craftitem("tobbaco:snuff", {
+    description = "Snuff",
+    inventory_image = "tobbaco_snuff.png",
+    groups = {tobbaco = 1},
+    on_use = function(itemstack, player, pointed_thing)
+    smokepos = player:get_pos()
+    minetest.chat_send_player(player:get_player_name(),"You take the snuff up your nose") 
+   
+--Plays one of two sounds at the player who smoked position
+    minetest.sound_play("snuff", {
+    pos = {x=smokepos.x, y=smokepos.y, z=smokepos.z},
+	max_hear_distance = 10,
+	gain = 6.0,
+})
+itemstack:take_item()
+    return itemstack
+end
+})
+minetest.register_craftitem("tobbaco:cigbutt", {
+    description = "Cigarette butt",
+    inventory_image = "tobbaco_cigbutt.png",
+    groups = {tobbaco = 1},
+})
 
 --"Cook" leaves into cured versions for crafting
 minetest.register_craft({
     type = "cooking",
-    output = "tobacco:leaves_cured",
-    recipe = "tobacco:leaves",
-    cooktime = 10,
+    output = "tobbaco:leaves_cured",
+    recipe = "tobbaco:leaves",
+    cooktime = 30,
 })
 -- Recipe for the cigarette
 minetest.register_craft({
-    type = "shapeless",
-    output = "tobacco:cigarette",
+    type = "shapeless", 
+    output = "tobbaco:cigarette",
     recipe = {
-        "tobacco:leaves_cured",
+        "tobbaco:leaves_cured",
         "default:paper",
+    },
+})
+
+-- Recipe for the snuff
+minetest.register_craft({
+    type = "shapeless", 
+    output = "tobbaco:snuff",
+    recipe = {
+        "tobbaco:leaves_cured",
+    },
+})
+
+-- Recipe using three cigarette butts to make a cigarette
+minetest.register_craft({
+    type = "shapeless", 
+    output = "tobbaco:cigarette",
+    recipe = {
+        "tobbaco:cigbutt",
+        "tobbaco:cigbutt",
+        "tobbaco:cigbutt",   
     },
 })
 
 -- Recipe to make seeds
 minetest.register_craft({
     type = "shapeless",
-    output = "tobacco:seed_leaves 3",
+    output = "tobbaco:seed_leaves 3",
     recipe = {
-        "tobacco:leaves",
+        "tobbaco:leaves",
     },
 })
 
@@ -82,8 +140,9 @@ for i = 1, 3 do
 	minetest.override_item("default:marram_grass_"..i, {drop = {
 		max_items = 1,
 		items = {
-			{items = {"tobacco:seed_leaves"},rarity = 10},
+			{items = {"tobbaco:seed_leaves"},rarity = 10},
 			{items = {"default:marram_grass_1"}},
 		}
 	}})
 end
+
